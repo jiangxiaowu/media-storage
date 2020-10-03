4e32bf18b6362fe17fa4511aa6ffdc696517ce2d
jiangxiaowu <jiangxiaowu6868@gmail.com>
2020-10-03 22:37:10 +0800

    home

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <TopNav />
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+    <v-snackbar>
+      <v-btn>
+        Close
+      </v-btn>
+    </v-snackbar>
+  </v-app>
+</template>
+
+<script>
+import TopNav from './components/TopNav.vue'
+
+export default {
+   name: 'app',
+  components: {
+    TopNav
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Header />
+    <Lists />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/home/Header.vue'
+import Lists from '@/components/home/Lists.vue'
+import Footer from '@/components/home/Footer.vue'
+
+export default {
+  components: {
+    Header,
+    Lists,
+    Footer
+  }
+}
+</script>
